946fc6cb84805d9dc6e07e56c05112967327b123
Ivan C <cordovaivan8@gmail.com>
2022-08-30 10:35:03 -0700

    added bootstrap

---
+++ script.js
@@ -0,0 +1,51 @@
+const questions = [
+    {
+        question: "What Does Html stand for?",
+        answers: {
+            1: "HyperText Markup Language",
+            2: "Home Text Makeup Language",
+            3: "HyperText Markup Link",
+            4: "HyperText document folder"
+        },
+        correctAnswer: "1"
+    },
+    {
+        question: "What year was javascript created?",
+        answers: {
+            1: "2001",
+            2: "2010",
+            3: "1995",
+            4: "1978"
+        },
+        correctAnswer: "3"
+    },
+    {
+        question: "What is css used for?",
+        answers: {
+            1: "To add elements to webpage",
+            2: "To add style to an html file",
+            3: "To add functionality to a webpage",
+            4: "To add text to a file"
+        },
+        correctAnswer: "2"
+    },
+    {
+        question: "What datatype has quotations around it?",
+        answers: {
+            1: "array",
+            2: "object",
+            3: "string",
+            4: "boolean"
+        },
+    },
+    {
+        question: "What goes at the end of a function?",
+        answers: {
+            1: ";",
+            2: "()",
+            3: ",",
+            4: "/"
+        },
+        correctAnswer: "2"
+    }
+];
