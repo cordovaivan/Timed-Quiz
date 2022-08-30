574ad55406217c4288e62cf04120fb339655a728
Ivan C <cordovaivan8@gmail.com>
2022-08-30 11:18:37 -0700

    added array of questions

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
-const questions = [
+let questions = [
     {
-        question: "What Does Html stand for?",
-        answers: {
-            1: "HyperText Markup Language",
-            2: "Home Text Makeup Language",
-            3: "HyperText Markup Link",
-            4: "HyperText document folder"
-        },
-        correctAnswer: "1"
+        question: "What does HTML stand for?",
+        choiceA: "HyperText Markup Language",
+        choiceB: "Home Text Makeup Language",
+        choiceC: "HyperText Markup Link",
+        choiceD: "HyperText document folder",
+        correct: "A"
     },
     {
         question: "What year was javascript created?",
-        answers: {
-            1: "2001",
-            2: "2010",
-            3: "1995",
-            4: "1978"
-        },
-        correctAnswer: "3"
+        choiceA: "2001",
+        choiceB: "2010",
+        choiceC: "1995",
+        choiceD: "1978",
+        correct: "C"
     },
     {
         question: "What is css used for?",
-        answers: {
-            1: "To add elements to webpage",
-            2: "To add style to an html file",
-            3: "To add functionality to a webpage",
-            4: "To add text to a file"
-        },
-        correctAnswer: "2"
+        choiceA: "To add elements to webpage",
+        choiceB: "To add style to an html file",
+        choiceC: "To add functionality to a webpage",
+        choiceD: "To add text to a file",
+        correct: "B"
     },
     {
-        question: "What datatype has quotations around it?",
-        answers: {
-            1: "array",
-            2: "object",
-            3: "string",
-            4: "boolean"
-        },
+        question: "What datatype has quotations around it",
+        choiceA: "array",
+        choiceB: "object",
+        choiceC: "string",
+        choiceD: "boolean",
+        correct: "C"
     },
     {
         question: "What goes at the end of a function?",
-        answers: {
-            1: ";",
-            2: "()",
-            3: ",",
-            4: "/"
-        },
-        correctAnswer: "2"
+        choiceA: ";",
+        choiceB: "()",
+        choiceC: ",",
+        choiceD: "/",
+        correct: "B"
     }
-];
+    ];
+
+let lastQuestionIndex = questions.length - 1;
+let runningQuestionIndex = 0;
+
+function renderQuestion() {
+    let q = questions[runningQuestionIndex];
+    question.innerHtml = "<p>" + q.question + "</p>";
+
+}
